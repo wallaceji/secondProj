2cfef7fa88af93f46c393741d47b70b0167c15c6
wallaceji <1002354488@qq.com>
2015-11-03 15:39:24 +0800

    this is the first changes
    I'm very happy to see this changes.

--- ImportData/Form1.cs
+++ ImportData/Form1.cs
@@ -15,6 +15,8 @@ namespace ImportData
 {
     public partial class Form1 : Form
     {
+    	//12345
+    	//34567
         //Data Source=10.1.0.68,1304;Initial Catalog=emrxk;Persist Security Info=True;User ID=newtouch_login;Password=newtouch_login_ps;Connect Timeout=900000
 
         //Data Source=10.1.0.109\newtouch;Initial Catalog=SpecialDiseaseDB;Persist Security Info=True;User ID=newtouch;Password=!newtouch123;Connect Timeout=900000
