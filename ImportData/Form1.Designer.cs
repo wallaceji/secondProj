5ce516dc6b4cc72ad84f7b8f8d1f862a960a5f08
wallaceji <1002354488@qq.com>
2015-11-03 15:34:14 +0800

    abc
    ggg

---
+++ ImportData/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ImportData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(792, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Data Source=10.1.0.158\\tech;Initial Catalog=EMRXKCS;Persist Security Info=True;Us" +
+                "er ID=sa;Password=winning;Connect Timeout=900000";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Begin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(48, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(792, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Data Source=10.1.0.158\\tech;Initial Catalog=EMRXKCS;Persist Security Info=True;Us" +
+                "er ID=sa;Password=winning;Connect Timeout=900000";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(153, 170);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(627, 105);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(48, 293);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 343);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ ImportData/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Compression;
+
+namespace ImportData
+{
+    public partial class Form1 : Form
+    {
+        //Data Source=10.1.0.68,1304;Initial Catalog=emrxk;Persist Security Info=True;User ID=newtouch_login;Password=newtouch_login_ps;Connect Timeout=900000
+
+        //Data Source=10.1.0.109\newtouch;Initial Catalog=SpecialDiseaseDB;Persist Security Info=True;User ID=newtouch;Password=!newtouch123;Connect Timeout=900000
+
+        //Data Source=10.1.0.158\tech;Initial Catalog=EMRXKCS;Persist Security Info=True;User ID=sa;Password=winning;Connect Timeout=900000
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection2 = new SqlConnection(textBox2.Text))
+                {
+                    connection2.Open();
+                    using (SqlConnection connection1 = new SqlConnection(textBox1.Text))
+                    {
+                        SqlDataReader myReader = null;
+                        connection1.Open();
+                        //SqlCommand cm = new SqlCommand(" select * from BL_QTBLJLK where cjsj>='" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + " 00:00:00' and cjsj<='" + dateTimePicker1.Value.ToString("yyyy-MM-dd") + " 23:59:59'");
+                        SqlCommand cm = new SqlCommand(" select * from BL_QTBLJLK where syxh=248767");
+                        cm.Connection = connection1;
+                        myReader = cm.ExecuteReader();
+
+                        SqlCommand cmdInsert = null;
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(" insert into T_COM_Record values (@syxh,@bldm,@blmc,@blms,@blnr,@cjys,@shys)");
+                        cmdInsert = new SqlCommand(sb.ToString());
+                        cmdInsert.Connection = connection2;
+                        while (myReader.Read())
+                        {
+                            cmdInsert.CommandText = sb.ToString();
+                            cmdInsert.Parameters.Clear();
+                            cmdInsert.Parameters.Add("syxh", SqlDbType.Int);
+                            cmdInsert.Parameters.Add("bldm", SqlDbType.VarChar);
+                            cmdInsert.Parameters.Add("blmc", SqlDbType.VarChar);
+                            cmdInsert.Parameters.Add("blms", SqlDbType.VarChar);
+                            cmdInsert.Parameters.Add("blnr", SqlDbType.VarChar);
+                            cmdInsert.Parameters.Add("cjys", SqlDbType.VarChar);
+                            cmdInsert.Parameters.Add("shys", SqlDbType.VarChar);
+
+                            cmdInsert.Parameters["syxh"].Value = int.Parse(myReader["syxh"].ToString());
+                            cmdInsert.Parameters["bldm"].Value = myReader["bldm"].ToString();
+                            cmdInsert.Parameters["blmc"].Value = myReader["blmc"].ToString();
+                            cmdInsert.Parameters["blms"].Value = myReader["blms"].ToString();
+                            cmdInsert.Parameters["blnr"].Value = UnzipEmrXml(myReader["blnr"].ToString());
+                            cmdInsert.Parameters["cjys"].Value = myReader["cjys"].ToString();
+                            cmdInsert.Parameters["shys"].Value = myReader["shys"].ToString();
+                            try
+                            {
+                                cmdInsert.ExecuteNonQuery();
+                            }catch(Exception eee){
+                                richTextBox1.Text += myReader["qtbljlxh"].ToString() + "导入出错\n";
+                            }
+                        }
+                    }
+                }
+                richTextBox1.Text += "ok \n";
+            }catch(Exception ee){
+                richTextBox1.Text += ee.Message;
+            }
+        }
+
+        private string UnzipEmrXml(string emrContent)
+        {
+            try
+            {
+                byte[] rbuff = Convert.FromBase64String(emrContent);
+                MemoryStream ms = new MemoryStream(rbuff);
+                DeflateStream dfs = new DeflateStream(ms, CompressionMode.Decompress, true);
+                StreamReader sr = new StreamReader(dfs, Encoding.UTF8);
+                string sXml = sr.ReadToEnd();
+                sr.Close();
+                dfs.Close();
+                return sXml;
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.WriteLine(e);
+                return emrContent;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
